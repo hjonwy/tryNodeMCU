1cb64eb9b686fff5ef981e1281d80227bfe93741
hjonwy <hjonwy@163.com>
2016-05-16 22:48:39 +0800

    Adust temperature as t/10.

--- Lua Programs/try_dht.lua
+++ Lua Programs/try_dht.lua
@@ -11,7 +11,7 @@ yeelink.init(347794,388717,"9a181908773eadd2cd4596a1f2be76d2",function()
 	
 	if(t) then
 		print("tempareture :" .. t .. ", update yeelink.")
-		yeelink.update(t)
+		yeelink.update(t/10)
 	else
 		print("dht is not ready")
 	end
