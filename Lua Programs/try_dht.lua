c8f6388eec5911902bedadf6c04e20e31d545b6e
John Hu <john.hu@carestream.com>
2016-05-19 17:13:24 +0800

    refactor dht.lua, remove dht_lib due to installed the latest nodmcu 1.4.0

--- Lua Programs/try_dht.lua
+++ Lua Programs/try_dht.lua
@@ -1,26 +1,87 @@
-dht = require("dht_lib")
+
 yeelink = require("yeelink_lib")
 
-yeelink.init(347794,388717,"9a181908773eadd2cd4596a1f2be76d2")
+local device = 347794
+local apiKey = "9a181908773eadd2cd4596a1f2be76d2"
+local tSensor=388717 --temperature sensor id
+local hSensor=388982 --humidity sensor id
 
-local sec = 10
+print("init yeelink")
+yeelink.init(device,apiKey)
 
-  tmr.alarm(1,12000,1,function()
-	print('read dht')
+local readDHTAndSubmit = function(curTime)
+  print('read dht')
+  
+  local status, temp, humi, temp_dec, humi_dec = dht.read(1)
 
-	dht.read(1)
+  if status ~= dht.OK then
+    print("dht is not ready")
+  else
+    print("tempareture :" .. temp)
+    print("humidity :" .. humi)
+    print("update yeelink.")
 
-	print('read dht done')
+    dataPoint = [[{"timestamp":]] .. curTime .. [[,"value":]]..temp
+    yeelink.addDatapoint(tSensor, dataPoint)
 
-	t = dht.getTemperature()
-	
-	if t == nil then
-        print("dht is not ready")	
-	else
-t=t/10
-	    print("tempareture :" .. t .. ", update yeelink.")
-        yeelink.update(t, sec)	
-		sec = sec+10
-	end
- 
+    --dataPoint = [[{"timestamp":]] .. curTime .. [[,"value":]]..humi
+    --yeelink.addDatapoint(hSensor, dataPoint)
+  end
+end
+
+local sk = net.createConnection(net.TCP, 0)
+
+local updateDatapoints = function()
+
+  local curTime = nil
+  local ip='23.21.227.249'
+  print("get current time from webapi.")
+
+  sk:on("connection", function(conn)
+    print("connect OK...")
+
+    sk:send([[GET http://www.timeapi.org/utc/now HTTP/1.1
+Host: www.timeapi.org
+Connection: keep-alive
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36
+Referer: http://www.timeapi.org/
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en-US,en;q=0.8
+Cookie: __utmt=1; __utma=56994735.1451350290.1463545272.1463624413.1463647328.4; __utmb=56994735.2.10.1463647328; __utmc=56994735; __utmz=56994735.1463624413.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)
+
+    
+    ]])
+
   end)
+
+  sk:on("receive", function(sck, c) print(c) curTime=c end)
+
+  sk:connect(80,ip)
+
+--  local cnt = 0
+--  tmr.alarm(3, 1000, 1, function()
+--    if (curTime == nil) and (cnt < 20) then
+--      print("curent time unavaiable, Waiting...")
+--      cnt = cnt + 1
+--    else
+--      tmr.stop(3)
+--      if (cnt < 20) then
+--        print("cur time is "..curTime)
+--        --readDHTAndSubmit(curTime)
+--      else
+--        print("get cur time fail due to overtime.")
+--      end
+--    end
+--  end)
+
+end
+
+--update datapoints to yeelink each 12 seconds
+print("start timer")
+if tmr.alarm(2,12000,1, updateDatapoints) then
+  print('tmr 2 is start')
+else
+  print('tmr 2 start failed.')
+end
--- Lua Programs/yeelink_lib.lua
+++ Lua Programs/yeelink_lib.lua
@@ -3,7 +3,7 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
-local dns = "42.96.164.52"
+local ip = "42.96.164.52" --yeelink ip
 
 local device = ""
 local sensor = ""
@@ -11,41 +11,27 @@ local apikey = ""
 
 local sk=net.createConnection(net.TCP, 0)
 
-local datapoint = 0
-
-function M.init(_device, _sensor, _apikey)
-    device = tostring(_device)
-    sensor = tostring(_sensor)
-    apikey = _apikey
+function M.init(_device, _apikey)
+  device = tostring(_device)
+  apikey = _apikey
 end
 
-function M.update(_datapoint, sec)
+function M.addDatapoint(sensor, datapoint)
 
-    datapoint = tostring(_datapoint)
-	--d=os.date("*t")
-	--timestamp=string.format("%x", d.year, d.month/10, d.month%10, d.day/10, d.day%10, d.hour/10, d.hour%10, d.min/10, d.min%10, d.sec/10, d.sec%10)
-  
-    sk:on("connection", function(conn) 
-        print("connect OK...") 
-    local key=[[{"timestamp":"2016-05-16T23:11:]]
-	local value=[[","value":]]
-	local value=[["value":]]
-    local e=[[}]]
-	
-    local st=key..sec..value..datapoint..e
-	local st=value..datapoint..e
+  sk:on("connection", function(conn)
+    print("connect OK...")
 
-        sk:send("POST /v1.0/device/"..device.."/sensor/"..sensor.."/datapoints HTTP/1.1\r\n"
-.."Host: www.yeelink.net\r\n"
-.."Content-Length: "..string.len(st).."\r\n"--the length of json is important
-.."Content-Type: application/x-www-form-urlencoded\r\n"
-.."U-ApiKey:"..apikey.."\r\n"
-.."Cache-Control: no-cache\r\n\r\n"
-..st.."\r\n" )
+    sk:send("POST /v1.0/device/"..device.."/sensor/"..sensor.."/datapoints HTTP/1.1\r\n"
+      .."Host: api.yeelink.net\r\n"
+      .."Content-Length: "..string.len(st).."\r\n"--the length of json is important
+      .."Content-Type: application/x-www-form-urlencoded\r\n"
+      .."U-ApiKey:"..apikey.."\r\n"
+      .."Cache-Control: no-cache\r\n\r\n"
+      ..datapoint.."\r\n" )
 
-    end)
+  end)
 
-    sk:connect(80,dns)
+  sk:connect(80,ip)
 
 end
 
