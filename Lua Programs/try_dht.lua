73e263e28f62b2b6f259999a9ac2c7a67f7045fa
hjonwy <hjonwy@163.com>
2016-05-19 23:12:25 +0800

    refactor tyr_dht, need to new a socket each time to call timer api.

--- Lua Programs/try_dht.lua
+++ Lua Programs/try_dht.lua
@@ -6,12 +6,15 @@ local apiKey = "9a181908773eadd2cd4596a1f2be76d2"
 local tSensor=388717 --temperature sensor id
 local hSensor=388982 --humidity sensor id
 
+local mainTimerId = 2
+local curTime = nil
+
 print("init yeelink")
 yeelink.init(device,apiKey)
 
-local readDHTAndSubmit = function(curTime)
+local readDHTAndSubmit = function()
   print('read dht')
-  
+
   local status, temp, humi, temp_dec, humi_dec = dht.read(1)
 
   if status ~= dht.OK then
@@ -21,67 +24,46 @@ local readDHTAndSubmit = function(curTime)
     print("humidity :" .. humi)
     print("update yeelink.")
 
-    dataPoint = [[{"timestamp":]] .. curTime .. [[,"value":]]..temp
-    yeelink.addDatapoint(tSensor, dataPoint)
+    --dataPoint = [[{"timestamp":]] .. curTime .. [[,"value":]]..temp
+    --yeelink.addDatapoint(tSensor, dataPoint)
 
     --dataPoint = [[{"timestamp":]] .. curTime .. [[,"value":]]..humi
     --yeelink.addDatapoint(hSensor, dataPoint)
   end
 end
 
-local sk = net.createConnection(net.TCP, 0)
+local request = "GET /timezoneJSON?lat=31.2&lng=121.59&username=hjonwy HTTP/1.1\r\n".."Host: api.geonames.org\r\n\r\n"
 
 local updateDatapoints = function()
 
-  local curTime = nil
-  local ip='23.21.227.249'
-  print("get current time from webapi.")
+  print("get current time from webapi. cur heap:" .. node.heap())
+  
+  local sk = net.createConnection(net.TCP, 0)
 
   sk:on("connection", function(conn)
-    print("connect OK...")
-
-    sk:send([[GET http://www.timeapi.org/utc/now HTTP/1.1
-Host: www.timeapi.org
-Connection: keep-alive
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36
-Referer: http://www.timeapi.org/
-Accept-Encoding: gzip, deflate, sdch
-Accept-Language: en-US,en;q=0.8
-Cookie: __utmt=1; __utma=56994735.1451350290.1463545272.1463624413.1463647328.4; __utmb=56994735.2.10.1463647328; __utmc=56994735; __utmz=56994735.1463624413.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)
-
-    
-    ]])
-
+    print("connect OK...")   
+    sk:send(request)
   end)
 
-  sk:on("receive", function(sck, c) print(c) curTime=c end)
+  sk:on("receive", function(sck, c)
+    print(c)
+    curTime=c
 
-  sk:connect(80,ip)
+    readDHTAndSubmit()
+    
+    tmr.start(mainTimerId)
+    sk:close()
+    sk=nil
+  end)
 
---  local cnt = 0
---  tmr.alarm(3, 1000, 1, function()
---    if (curTime == nil) and (cnt < 20) then
---      print("curent time unavaiable, Waiting...")
---      cnt = cnt + 1
---    else
---      tmr.stop(3)
---      if (cnt < 20) then
---        print("cur time is "..curTime)
---        --readDHTAndSubmit(curTime)
---      else
---        print("get cur time fail due to overtime.")
---      end
---    end
---  end)
+  sk:connect(80,"api.geonames.org")
 
 end
 
 --update datapoints to yeelink each 12 seconds
 print("start timer")
-if tmr.alarm(2,12000,1, updateDatapoints) then
-  print('tmr 2 is start')
+if tmr.alarm(mainTimerId,12000,1, function() tmr.stop(mainTimerId); updateDatapoints();  end) then
+  print('main timer is start')
 else
-  print('tmr 2 start failed.')
+  print('main timer start failed.')
 end
--- Lua Programs/yeelink_lib.lua
+++ Lua Programs/yeelink_lib.lua
@@ -3,11 +3,11 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
-local ip = "42.96.164.52" --yeelink ip
-
 local device = ""
-local sensor = ""
 local apikey = ""
+
+local sensor = nil
+local datapoint = nil
 
 local sk=net.createConnection(net.TCP, 0)
 
@@ -16,23 +16,28 @@ function M.init(_device, _apikey)
   apikey = _apikey
 end
 
-function M.addDatapoint(sensor, datapoint)
+sk:on("connection", function(conn)
+  print("connect OK...")
 
-  sk:on("connection", function(conn)
-    print("connect OK...")
+  sk:send("POST /v1.0/device/"..device.."/sensor/"..sensor.."/datapoints HTTP/1.1\r\n"
+    .."Host: api.yeelink.net\r\n"
+    .."Content-Length: "..string.len(st).."\r\n"--the length of json is important
+    .."Content-Type: application/x-www-form-urlencoded\r\n"
+    .."U-ApiKey:"..apikey.."\r\n"
+    .."Cache-Control: no-cache\r\n\r\n"
+    ..datapoint.."\r\n" )
 
-    sk:send("POST /v1.0/device/"..device.."/sensor/"..sensor.."/datapoints HTTP/1.1\r\n"
-      .."Host: api.yeelink.net\r\n"
-      .."Content-Length: "..string.len(st).."\r\n"--the length of json is important
-      .."Content-Type: application/x-www-form-urlencoded\r\n"
-      .."U-ApiKey:"..apikey.."\r\n"
-      .."Cache-Control: no-cache\r\n\r\n"
-      ..datapoint.."\r\n" )
-
-  end)
-
-  sk:connect(80,ip)
-
+end)
+  
+function M.addDatapoint(_sensor, _datapoint)
+  
+  if _datapoint ~= nil and _sensor ~= nil then
+    sensor = _sensor
+    datapoint = _datapoint
+ 
+    sk:connect(80,"api.yeelink.net")
+  end
+  
 end
 
 return M
